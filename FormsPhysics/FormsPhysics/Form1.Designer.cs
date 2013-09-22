8a55b0951aebfc9c0ebd89d8cf8ef3e9610ca9bb
Robikos <r.a.kostrzewski@gmail.com>
2013-09-22 22:00:44 +0200

    Symulator cząstki kwantowej
    Program prezentuje wykres prawdopodobieństwa występowania cząstki
    kwantowej w danym miejscu studni potencjału.

---
+++ FormsPhysics/FormsPhysics/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace FormsPhysics
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.InitialImage = null;
+            this.pictureBox1.Location = new System.Drawing.Point(13, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(300, 300);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 50;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(320, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Funkcja:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(323, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Brak";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(320, 64);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Dodaj:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(323, 80);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(430, 76);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Dodaj";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(323, 107);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(200, 173);
+            this.listBox1.TabIndex = 6;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(323, 289);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Clear";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(430, 289);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(93, 23);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Rysuj";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(445, 36);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(448, 53);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "label5";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(521, 76);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "start";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(530, 107);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 12;
+            this.button5.Text = "stop";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(608, 323);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Kwantowanie";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ FormsPhysics/FormsPhysics/Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormsPhysics
+{
+    public partial class Form1 : Form
+    {
+        int k = 0;
+        double t = 0.0;
+        double funkcjaR = 0.0;
+        double funkcjaI = 0.0;
+        double wynik = 0.0;
+        double maxwynik = 0.0;
+        Bitmap bufor = new Bitmap(300, 300);
+        Graphics g;
+        Pen p = new Pen(Color.Black);
+        Pen p2 = new Pen(Color.Black);
+        Point prev, next;
+        
+        int red,green,blue;
+
+
+        List<int> lista = new List<int>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            g = Graphics.FromImage(bufor);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            k++;
+            lista.Add(Int32.Parse(textBox1.Text));
+            listBox1.Items.Add("n =" + textBox1.Text);
+            if (label2.Text == "Brak")
+            {
+                label2.Text = "F(" + textBox1.Text + ") ";
+            }
+            else
+            {
+                label2.Text += "+ F(" + textBox1.Text + ") ";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            k = 0;
+            lista.Clear();
+            listBox1.Items.Clear();
+            label2.Text = "Brak";
+            maxwynik = 0.0;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            t += 0.05;
+            prev = new Point(0, 0);
+            g.Clear(Color.White);
+
+            for (int x = 1; x <= 300; x++)
+            {
+                funkcjaR = 0.0;
+                funkcjaI = 0.0;
+                wynik = 0.0;
+                foreach (int n in lista)
+                {
+                    funkcjaR += Math.Cos(0.577934 * n * t) * Math.Sin(0.01046 * n * x);
+                    funkcjaI += Math.Sin(0.577934 * n * t) * Math.Sin(0.01046 * n * x);
+                }
+                wynik = Math.Pow(funkcjaR, 2) + Math.Pow(funkcjaI, 2);
+                wynik = wynik / (double)(k*k);//normalizacja
+
+                if (wynik > maxwynik)
+                {
+                    maxwynik = wynik;
+                    label4.Text = maxwynik.ToString();
+                }
+                
+                toRGB(wynik*359.9, 0.8, 0.8); 
+                wynik = wynik * 300;
+
+                
+
+                next = new Point(x,300- (int)wynik);
+                g.DrawLine(p, prev, next);//rysowanie na buforze
+                p2.Color = Color.FromArgb(red, green, blue);
+                g.DrawLine(p2, next, new Point(x, 300));
+                prev = next;
+            }
+            pictureBox1.Image = bufor;
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            k = lista.Count;
+            label5.Text = k.ToString();
+            maxwynik = 0.0;
+            timer1.Start();
+        }
+
+        private void toRGB(double h, double s, double v)
+        {
+            if (v == 0)
+            {
+                red = 0;
+                green = 0;
+                blue = 0;
+            }
+            else
+            {
+                h = h / (double)60;
+                int i = (int)h;
+                double f = h - (float)i;
+                double p = v * (1 - s);
+                double q = v * (1 - (s * f));
+                double t = v * (1 - (s * (1 - f)));
+                if (i == 0) { red = (int)(v * 255); green = (int)(t * 255); blue = (int)(p * 255); }
+                else if (i == 1) { red = (int)(q * 255); green = (int)(v * 255); blue = (int)(p * 255); }
+                else if (i == 2) { red = (int)(p * 255); green = (int)(v * 255); blue = (int)(t * 255); }
+                else if (i == 3) { red = (int)(p * 255); green = (int)(q * 255); blue = (int)(v * 255); }
+                else if (i == 4) { red = (int)(t * 255); green = (int)(p * 255); blue = (int)(v * 255); }
+                else if (i == 5) { red = (int)(v * 255); green = (int)(p * 255); blue = (int)(q * 255); }
+
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+    }
+}
