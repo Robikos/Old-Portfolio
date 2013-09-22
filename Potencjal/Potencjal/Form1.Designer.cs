497a1a3efd3a2ee365fb79b4895b1eed66e015a0
Robikos <r.a.kostrzewski@gmail.com>
2013-09-22 22:07:41 +0200

    Powierzchnie ekwipotencjalne
    Prosty program symulujacy powierzchnie ekwipotencjalne, opisujace
    dzialanie konfigurowalnego pola elektrycznego.

---
+++ Potencjal/Potencjal/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace Potencjal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(13, 11);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(278, 283);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(299, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(135, 225);
+            this.listBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(299, 270);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(134, 24);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Usuń";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(299, 244);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(134, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(445, 306);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Symulator powierzchni ekwipotencjalnych";
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Potencjal/Potencjal/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Potencjal
+{
+    public partial class Form1 : Form
+    {
+        Graphics g;
+
+        int k = 10; //stala przyciagania
+        List<Mass> lista = new List<Mass>();
+
+        public class Mass
+        {
+            public int x;
+            public int y;
+            public int m;//masa
+            public int r;//promien na rysunku
+            public Mass(int x, int y, int m) { this.x = x; this.y = y; this.m = m; this.r = 5 * this.m;}
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            g = Graphics.FromImage(pictureBox1.Image);
+            //g = pictureBox1.CreateGraphics();
+            g.Clear(Color.White);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Random rnd = new Random();
+            //g.DrawLine(new Pen(new SolidBrush(Color.Black)),new Point(rnd.Next(pictureBox1.Width),rnd.Next(pictureBox1.Height)),new Point(rnd.Next(pictureBox1.Width),rnd.Next(pictureBox1.Height)));
+            //pictureBox1.Refresh();
+
+            if (listBox1.SelectedIndex >= 0)
+            {
+                lista.RemoveAt(listBox1.SelectedIndex);
+                reloadPicture();
+                reloadPotencjal();
+                pictureBox1.Refresh();
+                reloadBox();
+            }
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            Mass m = new Mass(e.Location.X, e.Location.Y, Int32.Parse(textBox1.Text));
+            lista.Add(m);
+            reloadPicture();
+            //tu bedzie reloadPicture() z wyliczeniem kropek i potem linii
+            reloadPotencjal();
+            pictureBox1.Refresh();
+            reloadBox();
+        }
+
+        private void reloadBox()
+        {
+            listBox1.Items.Clear();
+            for (int i = 0; i < lista.Count; i++)
+            {
+                listBox1.Items.Add("X: " + lista[i].x + "; Y: " + lista[i].y + "; M: " + lista[i].m);
+            }
+        }
+
+        private void reloadPicture()
+        {
+            g.Clear(Color.White);
+            for (int i = 0; i < lista.Count; i++)
+            {
+                Mass m = lista[i];
+                g.DrawEllipse(new Pen(new SolidBrush(Color.Blue)), m.x - m.r, m.y - m.r, 2 * m.r, 2 * m.r);
+            }
+        }
+
+        private double distance(int pX, int pY, int sX, int sY)
+        {
+            return Math.Sqrt((pX - sX) * (pX - sX) + (pY - sY) * (pY - sY));
+        }
+
+        private bool isFree(int pX, int pY)
+        {
+            for (int i = 0; i < lista.Count; i++)
+                if (distance(pX, pY, lista[i].x, lista[i].y) <= lista[i].r) return false;
+            return true;
+        }
+
+        private double potencjal(int pX, int pY)
+        {
+            double p = 0.0;
+            for (int i = 0; i < lista.Count; i++) p += k * lista[i].m / distance(pX, pY, lista[i].x, lista[i].y);
+            return p;
+        }
+
+        private void reloadPotencjal()
+        {
+            for(int x=0;x<pictureBox1.Width;x++)
+                for (int y = 0; y < pictureBox1.Height; y++)
+                {
+                    if (isFree(x, y))
+                    {
+                        //MessageBox.Show("" + potencjal(x, y));
+                        int k = (int)(100*potencjal(x, y));
+                        if (k % 10 <= 1 || k % 10 >= 8)
+                        {
+                            g.DrawEllipse(new Pen(new SolidBrush(Color.Black)), x, y, 1, 1);
+                        }
+                    }
+                }
+        }
+    }
+}
